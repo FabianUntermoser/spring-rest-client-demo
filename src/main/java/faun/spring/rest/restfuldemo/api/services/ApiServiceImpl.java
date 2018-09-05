bf79b3222e751c6cec4a7adf10bd7e23a23c61dc
Fabian Untermoser <f.untermoser@gmx.at>
2018-09-06 01:04:47 +0200

    [REFACTOR] Extract API Url to application.properties

--- src/main/java/faun/spring/rest/restfuldemo/api/services/ApiServiceImpl.java
+++ src/main/java/faun/spring/rest/restfuldemo/api/services/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package faun.spring.rest.restfuldemo.api.services;
 
 import faun.spring.rest.restfuldemo.api.domain.User;
 import faun.spring.rest.restfuldemo.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -11,14 +13,21 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService {
 
     private RestTemplate restTemplate;
+    private String apiUrl;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        return restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class).getData();
+
+        String restUrl = UriComponentsBuilder.fromUriString(apiUrl)
+                .queryParam("limit", limit)
+                .toUriString();
+
+        return restTemplate.getForObject(restUrl, UserData.class).getData();
     }
 
 }
