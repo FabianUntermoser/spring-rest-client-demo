0a3f459a51999b1222765fa43012518021300d37
Fabian Untermoser <f.untermoser@gmx.at>
2018-09-06 00:51:49 +0200

    [REFACTOR] mov2

---
+++ src/main/java/faun/spring/rest/restfuldemo/api/domain/Location.java
@@ -0,0 +1,54 @@
+
+package faun.spring.rest.restfuldemo.api.domain;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location implements Serializable {
+
+    private final static long serialVersionUID = -3532048267747973846L;
+    private String street, city, state, postcode;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
